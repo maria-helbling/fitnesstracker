86287cca95087bfa9bbea7aaec05d62d14e5b237
mahelb <65257133+maria-helbling@users.noreply.github.com>
2020-08-03 19:38:36 -0700

    charts added to stats page for three parameters

--- public/js/stats.js
+++ public/js/stats.js
@@ -2,30 +2,62 @@
 
 $.get('/api/workout', data=>{
     intensityMinutes(data);
+    countExercises(data);
+    typeRatio(data)
 })
 
-const intensityMinutes = (arr)=>{
-    let minuteObj = {}
+//type of exercise ratio
+const typeRatio = (arr)=>{
+    let typeArr = [0,0]
+    arr.forEach(element => {
+            typeArr[0]+= element.exercises.filter(ex=>ex.type === 'cardio').length
+            typeArr[1]+=element.exercises.filter(ex=>ex.type === 'strength').length
+    })
+    
+    let target = $('#type')
+    drawDoughnutChart(['darkgreen', 'goldenrod'],typeArr,target)
+}
+//number of exercises
+const countExercises = (arr)=>{
+    let exerciseObj = getDates()
+    
     arr.forEach(element => {
         let dt = moment(element.day).format('MM DD YYYY')
-        if (!minuteObj[dt]){
-            minuteObj[dt]=element.exercises.filter(element=>element.duration !== undefined).map(element => element = element.duration).reduce((total, num)=>total+num,0)
-        } else {
+            exerciseObj[dt]+=element.exercises.length
+    })
+    let numArr = []
+    let dates = []
+    for (let day in exerciseObj){
+        numArr.push(exerciseObj[day])
+        dates.push(day)
+    }
+
+    let target = $('#totalEx')
+    drawChart('goldenrod',numArr ,dates,target)
+}
+
+//intensity minutes this week
+const intensityMinutes = (arr)=>{
+    let minuteObj = getDates()
+    
+    arr.forEach(element => {
+        let dt = moment(element.day).format('MM DD YYYY')
             minuteObj[dt]+=element.exercises.filter(element=>element.duration !== undefined).map(element => element = element.duration).reduce((total, num)=>total+num,0)
-        }
-    });
+    })
+
     let minutes = []
     let dates = []
     for (let day in minuteObj){
         minutes.push(minuteObj[day])
         dates.push(day)
     }
+
     let target = $('#intensityMinutes')
-    drawChart('darkgreen',minutes ,target)
+    drawChart('darkgreen',minutes ,dates,target)
 }
 
-//draw little trend chart
-let drawChart = (chartColor, dataArr, appendTarget) =>{
+//draw bar chart
+let drawChart = (chartColor, dataArr,lableArr, appendTarget) =>{
     //set unique chart id
     let identifier = `val${dataArr[0]}`
     //put canvas on page
@@ -37,7 +69,7 @@ let drawChart = (chartColor, dataArr, appendTarget) =>{
     var myChart = new Chart(ctx, {
         type: 'bar',
         data: {
-            labels: [' ', ' ', ' ', ' ', ' ', ' ',' ',' ',' ',' ',' ',' ',' ',' '],
+            labels: lableArr,
             datasets: [{
                 label:'',
                 data: dataArr,
@@ -50,11 +82,11 @@ let drawChart = (chartColor, dataArr, appendTarget) =>{
             scales: {
                 yAxes: [{
                     ticks: {
-                        beginAtZero:false,
-                        display: false
+                        beginAtZero:true,
+                        display: true
                     },
                     gridLines: {
-                        display:false
+                        display:true
                     }
                 }],
                 xAxes:[{
@@ -67,10 +99,71 @@ let drawChart = (chartColor, dataArr, appendTarget) =>{
                 display: false
             },
             tooltips: {
-                enabled: false
+                enabled: true
             },
             maintainAspectRatio: false
             }
     })
 }
 
+//draw doughnut chart
+let drawDoughnutChart = (chartColor, dataArr, appendTarget) =>{
+    //set unique chart id
+    let identifier = `val${dataArr[0]}`
+    //put canvas on page
+    // let chartArea = $(`<div class="chart-container" id="datachart">`)
+    let chartArea =$(`<canvas id="${identifier}">`)
+    appendTarget.append(chartArea)
+    let ctx=$(`#${identifier}`);
+    //draw chart
+    var myChart = new Chart(ctx, {
+        type: 'doughnut',
+        data: {
+            labels: ['cardio', 'strength'],
+            datasets: [{
+                label:'',
+                data: dataArr,
+                backgroundColor: chartColor,
+                borderColor: chartColor,
+                borderWidth: 1
+            }]
+        },
+        options: {
+            scales: {
+                yAxes: [{
+                    ticks: {
+                        beginAtZero:true,
+                        display: false
+                    },
+                    gridLines: {
+                        display:false
+                    }
+                }],
+                xAxes:[{
+                    gridLines: {
+                        display:false
+                    },
+                    ticks: {
+                        beginAtZero:true,
+                        display: false
+                    }
+                }]
+            },
+            legend: {
+                display: true
+            },
+            tooltips: {
+                enabled: true
+            },
+            maintainAspectRatio: false
+            }
+    })
+}
+
+let getDates = () => {
+    let obj = {}
+    for (let i=0; i<7; i++) {
+        obj[moment().subtract(i,'d').format('MM DD YYYY')] = 0
+    }   
+    return obj
+}
